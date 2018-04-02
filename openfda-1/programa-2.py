a40b95367fb4d65ca71f63df6ce2a704c665af37
javipozo6 <javipozo14@gmail.com>
2018-04-02 11:53:05 +0200

    Programa 2 finalizado

--- openfda-1/programa-2.py
+++ openfda-1/programa-2.py
@@ -7,13 +7,11 @@ conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json?&limit=10", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
-repos_raw = r1.read().decode("utf-8")
+datos_raw = r1.read().decode("utf-8")
 conn.close()
 
-repo = json.loads(repos_raw)
+info = json.loads(datos_raw)['results']
 
-info = repo['results']
 
 for elem in range(len(info)):
     print('El id del medicamento es:', info[elem]['id'])
-    

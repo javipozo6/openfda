d7047a4be9025ef5552a144f351501e0caf812ba
javipozo6 <javipozo14@gmail.com>
2018-04-04 11:08:52 +0200

    Práctica 2 finalizada

--- openfda-2/aspirina.py
+++ openfda-2/aspirina.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json/?search=active_ingredient:acetylsalicylic&limit=100", None, headers)
+conn.request("GET", "/drug/label.json/?search=active_ingredient:acetylsalicylic&limit=100", None, headers) #Realizamos la búsqueda comprobando el principio activo y poniendo un límite de 100
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 datos_raw = r1.read().decode("utf-8")
@@ -13,9 +13,10 @@ conn.close()
 info = json.loads(datos_raw)['results']
 
 
-for elem in range(len(info)):
-    print('El id del medicamento es:', info[elem]['id'])
+for elem in range(len(info)): #Con esto iteramos sobre los datos de los diferentes medicamentos que tenemos
+    print('El id del medicamento es:', info[elem]['id']) #Accedemos al id del medicamento
+    #Si el nombre del fabricante es desconocido, saltará un KeyError, asi que lo evitamos con un try-except
     try:
-        print('El nombre del fabricante es:', info[elem]['openfda']['manufacturer_name'],'\n')
+        print('Su fabricante es:', info[elem]['openfda']['manufacturer_name'][0],'\n')
     except KeyError:
         print('El nombre del fabricante no está disponible\n')

d831d6da0843f03cfef465e663986c4515b4972a
javipozo6 <javipozo14@gmail.com>
2018-04-06 11:20:48 +0200

    Programa 2 finalizado.

--- openfda-2/aspirina.py
+++ openfda-2/aspirina.py
@@ -6,11 +6,9 @@ headers = {'User-Agent': 'http-client'}
 conn = http.client.HTTPSConnection("api.fda.gov") #Establecemos conexión con la página solicitada
 conn.request("GET", "/drug/label.json/?search=active_ingredient:acetylsalicylic&limit=100", None, headers)#GET: enviamos solicitud y realizamos la búsqueda comprobando el principio activo y poniendo un límite de 100
 r1 = conn.getresponse() #Obtenemos la respuesta de la solicitud enviada
-
 if r1.status == 404: #Comprobamos si se ha podido encontrar el recurso correctamente (código 200), o si no se ha podido (código 404)
     print("ERROR, recurso no encontrado.")
     exit(1)
-
 print(r1.status, r1.reason) #Imprimimos el código de estado de la respuesta
 datos_raw = r1.read().decode("utf-8") #Convertimos la información para que sea legible
 conn.close()
